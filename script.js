54fbdd92c4fe4ca4b5440e25f74398a338ccb881
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-08 10:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const lookup = {
   'M': 'Z','N': 'A','O': 'B','P': 'C',
   'Q': 'D','R': 'E','S': 'F','T': 'G',
   'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
+  'Y': 'L','Z': 'M', '?': '?', ',': ',',' ': ' '
 };
 
 function rot13(encodedStr){
@@ -15,9 +15,7 @@ function rot13(encodedStr){
   // Only change code below this line
 
 	for (let i = 0; i < encodedStr.length; i++) {
-		if (encodedStr[i] >= 'A' && encodedStr[i] <= 'Z') {
 			decodedArr.push(lookup[encodedStr[i]]);
-		}
 	}
   return decodedArr.join("");
 }
