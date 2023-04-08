50e93d6b12d1632e875f33b9ca4ee416a4bc29aa
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-08 10:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function rot13(encodedStr){
   // Only change code below this line
 
 	for (let i = 0; i < encodedStr.length; i++) {
-		if (encodedStr[i] >= 65 && encodedStr[i] <= 90) {
+		if (encodedStr[i] >= 'A' && encodedStr[i] <= 'Z') {
 			decodedArr.push(lookup[encodedStr[i]]);
 		}
 	}
